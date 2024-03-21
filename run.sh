2133c1948fb4c13bdf60e5aa1bd73698cdc1e6c1
iduyeth <164357163+iduyeth@users.noreply.github.com>
2024-03-21 21:30:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}'https://eth1.lava.build/lava-referer-86e02ffc-e3da-4e02-acce-751a917bbf1a/ )
 
 
 echo "$curl_output" >> Update.tsx
